d99aeda2be8481e8545459843d846dca3e3a78fa
Suresh Maurya <sureshmaurya48@gmail.com>
2023-03-09 17:18:00 +0530

    added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://suresh48:951Suresh@shoping-crud.42q7ak0.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://avatar:951Suresh@cluster0.3bg1qmu.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>console.log('connected'))
 .catch(e=>console.log('error:',e))
